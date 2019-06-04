b7403fdace8da18ae230e4ec786dafdc8f341758
Kyle-Pu <kylez.pu@gmail.com>
2019-06-04 15:40:01 -0700

    Handling missing data values. Simulated with empty data values in the spreadsheet.

--- projectProcessing.py
+++ projectProcessing.py
@@ -1,17 +1,21 @@
 import pandas as pd
 signups = pd.read_excel("signups.xlsx", sheet_name = "Sheet1")
 
+# Take care of missing values
+print("\nThe following people need to be contacted because they didn't provide a complete response")
+print(signups.apply(lambda x: print(signups[x.isnull()].Name.to_string(index=False))))
+
 print("\nMESA Machine Signups: ")
-print(signups.loc[signups.Machine == 1].Name.to_string(index = False))
+print(signups[signups.Machine == 1].Name.to_string(index=False))
 
 print("\nGlider Signups:")
-print(signups.loc[signups.Glider == 1].Name.to_string(index=False))
+print(signups[signups.Glider == 1].Name.to_string(index=False))
 
 print("\nTank Signups:")
-print(signups.loc[signups.Tank == 1].Name.to_string(index=False))
+print(signups[signups.Tank == 1].Name.to_string(index=False))
 
 print("\nBridge Signups:")
-print(signups.loc[signups.Bridge == 1].Name.to_string(index=False))
+print(signups[signups.Bridge == 1].Name.to_string(index=False))
 
 print("\nArm Signups:")
-print(signups.loc[signups.Arm == 1].Name.to_string(index=False))
+print(signups[signups.Arm == 1].Name.to_string(index=False))
